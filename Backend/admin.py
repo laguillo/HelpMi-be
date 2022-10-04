07b4bfaf817eea186b8bbb1982dda03058696556
Laguillo <laguillo@gmail.com>
2022-10-04 10:31:52 -0500

    admin

--- Backend/admin.py
+++ Backend/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 
-from .models.historiaClinica import Historia
-from .models.familiar import Familiar
+# from .models.historiaClinica import Historia
+# from .models.familiar import Familiar
 from .models.user import User
-from .models.medico import Medico
-from .models.paciente import Paciente
+# from .models.medico import Medico
+# from .models.paciente import Paciente
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Medico)
-admin.site.register(Paciente)
-admin.site.register(Historia)
-admin.site.register(Familiar)
+# admin.site.register(Medico)
+# admin.site.register(Paciente)
+# admin.site.register(Historia)
+# admin.site.register(Familiar)
--- Backend/views/medicoviews.py
+++ Backend/views/medicoviews.py
@@ -1,38 +1,38 @@
-# from rest_framework import *
-# from rest_framework.response import Response
-# from Backend.serializers.userSerializer import UserSerializer
+from rest_framework import *
+from rest_framework.response import Response
+from Backend.serializers.userSerializer import UserSerializer
 
-# from rest_framework import APIView
-# from rest_framework import status
-# from rest_framework.response import Response
-
-
-# from Backend.models import medico
-# from Backend.serializers.medicoSerializer import MedicoSerializer
+from rest_framework import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
 
-# class medicoView(APIView):
-#     serializer_class = MedicoSerializer
+from Backend.models import medico
+from Backend.serializers.medicoSerializer import MedicoSerializer
 
-#     def get(self, request, *args,**kwargs):
-#         medicos = medico.objects.filter(id=kwargs['NunMedico'])
-#         medico_serializer = MedicoSerializer(medico,many=True)
-#         return Response(medico_serializer.data)
 
-# class medioCrear(APIView):
+class medicoView(APIView):
+    serializer_class = MedicoSerializer
 
-#     serializer_class = MedicoSerializer
+    def get(self, request, *args,**kwargs):
+        medicos = medico.objects.filter(id=kwargs['NunMedico'])
+        medico_serializer = MedicoSerializer(medico,many=True)
+        return Response(medico_serializer.data)
 
-#     def get(self, request, *args,**kwargs): 
-#         medicos=medico.objects.all()
-#         medico_serializer = MedicoSerializer(medico,many=True)
-#         return Response(medico_serializer.data)
+class medioCrear(APIView):
 
-#     def post(self, request, *args, **kwargs):
-#         medico_serializer=MedicoSerializer(data=request.data)
-#         medico_serializer.is_valid(raise_exception=True)
-#         medico_serializer.save()
-#         return Response(medico_serializer.data,status=status.HTTP_201_CREATED)
+    serializer_class = MedicoSerializer
+
+    def get(self, request, *args,**kwargs): 
+        medicos=medico.objects.all()
+        medico_serializer = MedicoSerializer(medico,many=True)
+        return Response(medico_serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        medico_serializer=MedicoSerializer(data=request.data)
+        medico_serializer.is_valid(raise_exception=True)
+        medico_serializer.save()
+        return Response(medico_serializer.data,status=status.HTTP_201_CREATED)
 
 
 
