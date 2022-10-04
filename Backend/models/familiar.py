c1702dcbbdac9e2d1d87848c921fb201693f093e
Christian Vega <cvmesa29@gmail.com>
2022-10-04 11:41:12 -0500

    Cambios en imports y related_names

--- Backend/models/familiar.py
+++ Backend/models/familiar.py
@@ -1,18 +1,19 @@
 from django.db import models
 
-from .historiaClinica import Historia
-from .paciente import Paciente
-from .user import User, models
+
+
+from .user import User
 
 class Familiar(models.Model):
     id=models.AutoField(primary_key=True)
-    usuario=models.ForeignKey(User, related_name='familiar', on_delete=models.CASCADE)
-    paciente=models.ForeignKey(Paciente, related_name='paciente', on_delete=models.CASCADE)
+    usuario=models.ForeignKey(User, on_delete=models.CASCADE)
     
-    oximetria = models.DecimalField('Oximetria',max_digits=None,decimal_places=None)
-    f_respiratoria = models.DecimalField('Frecuencia respiratoria', max_digits=None,decimal_places=None)
-    f_cardiaca = models.DecimalField('Frecuencia cardiaca',max_digits=None,decimal_places=None)
-    temperatura = models.DecimalField('Temperatura', max_digits=None,decimal_places=None)
-    p_arterial = models.DecimalField('Presion arterial', max_digits=None,decimal_places=None)
-    glicemias = models.DecimalField('Glicemias',max_digits=None,decimal_places=None)
+    
+
+    oximetria = models.FloatField('Oximetria')
+    f_respiratoria = models.FloatField('Frecuencia respiratoria')
+    f_cardiaca = models.FloatField('Frecuencia cardiaca')
+    temperatura = models.FloatField('Temperatura')
+    p_arterial = models.FloatField('Presion arterial')
+    glicemias = models.FloatField('Glicemias')
     
--- Backend/models/paciente.py
+++ Backend/models/paciente.py
@@ -2,12 +2,12 @@ from django.db import models
 
 from .familiar import Familiar
 from .user import User
-from .medico import Medico
+
 
 #OneToOneField
 
 class Paciente(models.Model):
     id=models.AutoField(primary_key=True)
-    usuario=models.ForeignKey(User, related_name='paciente', on_delete=models.CASCADE)
-    medico=models.ForeignKey(Medico, related_name='paciente', on_delete=models.CASCADE)
-    familiar=models.ForeignKey(Familiar, related_name='paciente', on_delete=models.CASCADE)
+    usuario_p=models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    familiar=models.ForeignKey(Familiar, on_delete=models.CASCADE)
