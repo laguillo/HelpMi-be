7a160cdbd00088c63abfceb22ba5c75a766f527f
Christian Vega <cvmesa29@gmail.com>
2022-10-04 12:41:48 -0500

    Changes

--- Backend/models/familiar.py
+++ Backend/models/familiar.py
@@ -1,14 +1,8 @@
 from django.db import models
-
-
-
 from .user import User
-
 class Familiar(models.Model):
     id=models.AutoField(primary_key=True)
-    usuario=models.ForeignKey(User, on_delete=models.CASCADE)
-    
-    
+    usuario=models.ForeignKey(User, on_delete=models.CASCADE)    
 
     oximetria = models.FloatField('Oximetria')
     f_respiratoria = models.FloatField('Frecuencia respiratoria')
--- Backend/models/medico.py
+++ Backend/models/medico.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from .historiaClinica import Historia
-from .user import User, models
+from .user import User
 
 class Medico(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,4 +9,3 @@ class Medico(models.Model):
         User, on_delete=models.CASCADE)
     especialidad = models.CharField('Especialidad', max_length=45)
     diagnostico = models.CharField('Diagnostico', max_length=400)
-    historia = models.ForeignKey(Historia, related_name='paciente', on_delete=models.CASCADE)
