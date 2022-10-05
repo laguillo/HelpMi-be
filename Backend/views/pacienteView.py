6dc0237fa4f90c763ed6b84c40d35d1504a3e477
Laguillo <laguillo@gmail.com>
2022-10-05 09:44:44 -0500

    fix

--- Backend/views/pacienteView.py
+++ Backend/views/pacienteView.py
@@ -4,31 +4,33 @@ from Backend.models.paciente import Paciente
 from serializers.pacienteSerializer import PacienteSerializer
 from rest_framework.decorators import api_view
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def createpaciente(request):
     if request.method == 'GET':
-        modelo=Paciente.objects.all()
-        serializer=PacienteSerializer(modelo, many=True)
+        modelo = Paciente.objects.all()
+        serializer = PacienteSerializer(modelo, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        serializer=PacienteSerializer(data=request.data)
+        serializer = PacienteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT','DELETE'])
-def detailpaciente(request,pk):
-    if request.method =='PUT':
-        modelo=Paciente.objects.get(pk=pk)
-        serializer=PacienteSerializer(modelo, data=request.data)
+
+@api_view(['PUT', 'DELETE'])
+def detailpaciente(request, pk):
+    if request.method == 'PUT':
+        modelo = Paciente.objects.get(pk=pk)
+        serializer = PacienteSerializer(modelo, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
-        modelo=Paciente.objects.get(pk=pk)
+        modelo = Paciente.objects.get(pk=pk)
         modelo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
