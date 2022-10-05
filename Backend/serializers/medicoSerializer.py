7ce73b4af612021485bf99b2c08ccdab5a56323c
Laguillo <laguillo@gmail.com>
2022-10-05 10:05:33 -0500

    fix

--- Backend/serializers/medicoSerializer.py
+++ Backend/serializers/medicoSerializer.py
@@ -4,31 +4,8 @@ from Backend.models.medico import Medico
 class MedicoSerializer(serializers.ModelSerializer):
     class Meta:
         model= Medico
-        fields = '__all__'
-
-    def to_representation(self, instance):
-        return {
-            'Nombre Medico : ': instance.usuario_m.nombre,
-            'Apellido Medico : ': instance.usuario_m.apellido,
-            'Especialidades : ': instance.id.especialidad,
-            'Diagnosticos : ': instance.id.diagnostico,
-            
-            'Paciente : ':{
-            'Nombre Paciente : ': instance.historia.paciente.usuario.nombre,
-            'Apellido Paciente : ': instance.historia.paciente.usuario.nombre,
-            }
-            # 'Historia : ':{
-                
-            #     'Oximetría : ': instance.historia.familiar.oximetria,
-            #     'Frecuencia respiratoria : ': instance.historia.familiar.f_respiratoria,
-            #     'Frecuencia cardiaca : ': instance.historia.familiar.f_cardiaca,
-            #     'temperatura : ': instance.historia.familiar.temperatura,
-            #     'Presion Arterial : ': instance.historia.familiar.p_arterial,
-            #     'Glicemias : ': instance.historia.familiar.glicemias
-
-            #  }
-
-        }
+        fields = ['usuario', 'especialidad']
+        
 
 
 
