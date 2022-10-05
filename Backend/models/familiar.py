11e7bdbb13702770b17d14821a101a8eaa0fadaf
Laguillo <laguillo@gmail.com>
2022-10-05 07:13:20 -0500

    fix familiar

--- Backend/models/familiar.py
+++ Backend/models/familiar.py
@@ -5,4 +5,4 @@ from .user import User
 class Familiar(models.Model):
     id = models.AutoField(primary_key=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    paciente = models.CharField('Nombre del paciente')
+    nombre_paciente = models.CharField('Nombre del paciente', max_length=(50))
